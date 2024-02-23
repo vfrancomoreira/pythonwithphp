c4b1c79afbbd661deb78dd7f4ef4461e47ac6cfc
Vinícius Franco <viniciusfsantosm15@gmail.com>
2024-02-23 17:53:40 -0300

    Subindo arquivos .py

---
+++ python/bot.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Edge()
+driver.get('https://google.com/')
+time.sleep(10)
+
+# API DO EDITA CÓDIGO
+
+tokenApi = 'yfBH6qM0vzcc7ZtdAb1qGgg7uE2rS7zU'
